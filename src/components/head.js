a5b52d76e8e7d4c10a497839883601eb08a2ea98
akzhy <akshay48884@gmail.com>
2019-10-06 20:49:42 +0530

    Start

---
+++ src/components/head.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+export default () => {
+    return (
+        <Helmet>
+            <link href="https://fonts.googleapis.com/css?family=Poppins:400,900" rel="stylesheet"/>>
+        </Helmet>
+    )
+}
