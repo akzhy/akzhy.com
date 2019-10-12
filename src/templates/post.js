ce783f93b5c0ce1ebc85c50bac5cf3fc063c730e
akzhy <akshay48884@gmail.com>
2019-10-12 21:17:49 +0530

    Posts page

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,7 +1,74 @@
-import React from "react";
+import React from "react"
+import { graphql } from "gatsby"
+import Prism from "prismjs";
 
-export default () => {
-    return(
-        <h1>Hello</h1>
+import Title from "../components/title"
+import Layout from "../components/layout"
+
+import "../styles/prism.css"
+import "../styles/page.less"
+
+export default ({ data }) => {
+    const post = data.wordpressPost;
+
+    const seo = {
+        title: post.title,
+        description:  post.acf.meta_description,
+        image: post.featured_media.localFile.childImageSharp.fluid.src
+    }
+
+    return (
+        <Layout seo={seo}>
+            <div className="section">
+                <div className="container">
+                    <div className="meta">
+                        <Title data={post.title}/>
+                        <p className="date">{post.date}</p>
+                        <p className="date">{post.modified}</p>
+                    </div>
+                    <div className="page">
+                        <CodeHighlighter content={ post.content }/>
+                    </div>
+                </div>
+            </div>
+        </Layout>
     )
 }
+
+class CodeHighlighter extends React.Component{
+    componentDidMount(){
+        setTimeout(() => Prism.highlightAll(), 10);
+    }
+
+    render(){
+        return (
+            <div dangerouslySetInnerHTML={{ __html: this.props.content }} />
+        )
+    }
+}
+
+export const query = graphql`
+    query($id: String!) {
+        wordpressPost(id: {eq: $id}) {
+            title
+            categories {
+                name
+            }
+            featured_media {
+                localFile {
+                    childImageSharp {
+                        fluid {
+                            src
+                        }
+                    }
+                }
+            }
+            modified(formatString: "DD MMM YYYY")
+            content
+            date(formatString: "DD MMM YYYY")
+            acf {
+                meta_description
+            }
+        }
+    }
+`
