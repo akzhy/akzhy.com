e86f2f955db2aa02e1d0686c78822b4d94f5f20e
akzhy <akshay48884@gmail.com>
2019-10-12 12:25:25 +0530

    Blog page

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -1,7 +1,71 @@
-import React from "react";
+import React from "react"
+import Layout from "../components/layout"
+import Title from "../components/title"
+import { BlogItem } from "../components/items"
+import { graphql } from "gatsby"
 
-export default () => {
+const seo = {
+    title: "Blog",
+    description: ""
+}
+
+export default ({ data, pageContext }) => {    
+
+    if(pageContext.currentPage !== 1){
+        seo.title = `Blog, page ${pageContext.currentPage}`
+    }
+
+    const blogItems = [];
+    data.allWordpressPost.edges.forEach((item,i) => {
+        blogItems.push(
+            <BlogItem title={item.node.title} description={item.node.acf.description} date={item.node.date} link={item.node.path} image={item.node.featured_media.localFile.childImageSharp.fluid} id={item.node.id} key={item.node.id}/>
+        )
+    })
     return (
-        <h1>Hello</h1>
+        <Layout seo={seo}>
+            <div className="section" id="blog">
+                <div className="container">
+                    <Title data="Blog"/>
+                    <div className="contents">
+                        <div className="row">
+                            {blogItems}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Layout>
     )
 }
+
+export const query = graphql`
+    query($skip: Int!, $limit: Int!){
+        allWordpressPost(
+            filter: { categories: { elemMatch: { slug: { eq: "blog" } } } }
+            limit: $limit,
+            skip: $skip
+        ) {
+            edges {
+                node {
+                    acf{
+                        description
+                    }
+                    id
+                    date(formatString: "DD MMM YYYY")
+                    title
+                    path
+                    featured_media {
+                        source_url
+                        localFile {
+                            childImageSharp {
+                                fluid(maxWidth: 600) {
+                                    src
+                                    ...GatsbyImageSharpFluid_noBase64
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
