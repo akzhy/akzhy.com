4b81da4b1c33a09a5e883ac0f564b1f17f393ce3
akzhy <akshay48884@gmail.com>
2019-10-28 18:04:31 +0530

    Subscription listing

--- src/pages/manage-subscription.js
+++ src/pages/manage-subscription.js
@@ -3,8 +3,9 @@ import Layout from "../components/layout"
 import config from "../../config.json"
 import Title from "../components/title"
 
+
 const seo = {
-    title: "Login",
+    title: "Manage Subscriptions",
 }
 export default class Login extends React.Component {
     constructor(props) {
@@ -14,7 +15,8 @@ export default class Login extends React.Component {
 
         this.state = {
             hasToken: false,
-            searchParams: {}
+            searchParams: {},
+            subscriptions: false
         }
     }
 
@@ -27,7 +29,28 @@ export default class Login extends React.Component {
                     token: params.get("token")
                 }
             })
+            const headers = {
+                'Content-Type': 'application/json'
+            }
+            fetch(`${config.cms}/wp-json/restcommentsubscribing/v1/manage`,
+            {
+                method: "post",
+                body: JSON.stringify({
+                    token:params.get("token")
+                }),
+                headers: headers,
+            }).then((response) => {
+                return response.json();
+            }).then(obj => {
+                if(obj.result === "success"){
+                    const data = JSON.parse(obj.data);
+                    this.setState({
+                        subscriptions: data
+                    })
+                }
+            })
         }
+        
     }
 
     submit(event) {
@@ -60,6 +83,9 @@ export default class Login extends React.Component {
                     <div className="container">
                         <div className="boxed">
                             <Title data="Manage Subscriptions" />
+                            {this.state.subscriptions && 
+                                <Subscriptions data={this.state.subscriptions}/>
+                            }
                             {!this.state.hasToken && (
                                 <form onSubmit={this.submit}>
                                     <p>To manage your subscriptions, please enter your email address.</p>
@@ -85,6 +111,61 @@ export default class Login extends React.Component {
                     </div>
                 </div>
             </Layout>
+        )
+    }
+}
+
+const Subscriptions = ({data}) => {
+    
+    const list = [];
+
+    data.forEach((item,index) => {
+        list.push(
+            <Row key={"tr"+index} data={item}/>
+        )
+    })
+    
+    return (
+        <table>
+            <thead>
+            <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Post</th>
+                <th>Comment</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+                {list}
+            </tbody>
+        </table>
+    )
+}
+
+class Row extends React.Component{
+
+    componentDidMount(){
+
+    }
+
+    unsub(){
+
+    }
+
+    render(){
+        const data = this.props.data;
+        
+        return (
+            <tr className="subscription-item">
+                <td>
+                    <input type="checkbox" name="select-subscriptions"/>
+                </td>
+                <td>{data.name}</td>
+                <td><a href={data.post.slug} title={data.post.title}>{data.post.title}</a></td>
+                <td><a href={`${data.post.slug}#c${data.comment.id}`}>{data.comment.content}</a></td>
+                <td><a href="#unsub">Unsubscribe</a></td>
+            </tr>
         )
     }
 }
