d0a40185a9941f6aa3053dfd9028eefb46e6ef79
akzhy <akshay48884@gmail.com>
2019-11-03 19:36:34 +0530

    Sidebar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,57 +1,203 @@
-import React from "react";
-import { Link } from "gatsby";
-import Logo from "./logo";
+import React from "react"
+import { Link } from "gatsby"
+import Logo from "./logo"
+import Sidebar from "react-sidebar"
+import SocialLinks from "./social"
 
-export default () => {
+const SidebarContents = () => {
     const navLinks = [
         {
-            name: "About",
-            url: "/about"
-        },{
-            name: "Shelf",
-            url: "/shelf"
-        },{
             name: "Home",
             url: "/",
-            logo: true,
-        },{
+        },
+        {
+            name: "About",
+            url: "/about",
+        },
+        {
+            name: "Shelf",
+            url: "/shelf",
+        },
+        {
             name: "Blog",
-            url: "/blog"
-        },{
+            url: "/blog",
+        },
+        {
             name: "Contact",
-            url: "/contact"
-        }
-    ];
+            url: "/contact",
+        },
+    ]
 
-    const list = [];
+    const list = []
 
-    navLinks.forEach((item,i) => {
+    navLinks.forEach((item, i) => {
+        if (item.sidebar) return
         list.push(
-            <NavLink data={item} key={item.name+""+item.url}/>
+            <NavLink data={item} key={"sidenav" + item.name + "" + item.url} />
         )
     })
-
-    return(
-        <nav>
-            <div className="container">
-                <ul>
-                    {list}
-                </ul>
+    return (
+        <div className="sidebar-contents">
+            <div className="logo">
+                <Link to="/">
+                    <Logo />
+                </Link>
             </div>
-        </nav>
+            <ul className="links text-secondary">{list}</ul>
+            <div className="social-links">
+                <SocialLinks />
+            </div>
+        </div>
     )
 }
 
-const NavLink = ({data}) => {
+export default class Navbar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            navbarPlaceholderHeight: 100,
+            sidebarOpen: false,
+        }
+
+        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this)
+        this.menuOpen = this.menuOpen.bind(this)
+
+        this.navbar = React.createRef()
+    }
+
+    windowOnScroll = e => {
+        const top = window.pageYOffset || document.documentElement.scrollTop
+        if (top > 10) {
+            this.navbar.current.classList.add("scrolled")
+        } else {
+            this.navbar.current.classList.remove("scrolled")
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener("scroll", this.windowOnScroll)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.windowOnScroll)
+    }
+
+    onSetSidebarOpen(open) {
+        this.setState({ sidebarOpen: open })
+    }
+
+    menuOpen(event) {
+        event.preventDefault()
+        this.onSetSidebarOpen(true)
+    }
+
+    render() {
+        const navLinks = [
+            {
+                name: "About",
+                url: "/about",
+            },
+            {
+                name: "Shelf",
+                url: "/shelf",
+            },
+            {
+                name: "Home",
+                url: "/",
+                logo: true,
+            },
+            {
+                name: "Blog",
+                url: "/blog",
+            },
+            {
+                name: "Contact",
+                url: "/contact",
+            },
+        ]
+
+        const list = []
+
+        navLinks.forEach((item, i) => {
+            list.push(<NavLink data={item} key={item.name + "" + item.url} />)
+        })
+
+        return (
+            <React.Fragment>
+                <div className="sidebar-container">
+                    <Sidebar
+                        sidebar={<SidebarContents />}
+                        open={this.state.sidebarOpen}
+                        onSetOpen={this.onSetSidebarOpen}
+                        sidebarClassName="sidebar-content"
+                        styles={{
+                            sidebar: {
+                                zIndex: 101,
+                                position: "fixed"
+                            },
+                            overlay: {
+                                zIndex: 100
+                            },
+                            dragHandle: {
+                                position: "fixed",
+                                zIndex: "99999"
+                            }
+                        }}
+                    >
+                        <span></span>
+                    </Sidebar>
+                </div>
+                <nav ref={this.navbar}>
+                    <div className="container">
+                        <ul className="main-nav">{list}</ul>
+                        <ul className="mobile-nav">
+                            <li className="menu-button" onClick={() => this.onSetSidebarOpen(true)}>
+                                <div className="hamburger">
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
+                                </div>
+                            </li>
+                            <li>
+                                <Logo />
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </React.Fragment>
+        )
+        /* return (
+            <React.Fragment>
+                <Sidebar
+                    sidebar={<SidebarContents />}
+                    open={this.state.sidebarOpen}
+                    onSetOpen={this.onSetSidebarOpen}
+                    sidebarClassName="sidebar-content"
+                >
+                    <span></span>
+                </Sidebar>
+                <nav className="text-secondary" ref={c => (this.nav = c)}>
+                    <a href="#mobilenav" id="menu-open" onClick={this.menuOpen}>
+                        <span className="icon">
+                            <Hamburger />
+                        </span>
+                    </a>
+                    <Link to="/">
+                        <Logo />
+                    </Link>
+                    <NavLinks />
+                </nav>
+            </React.Fragment>
+        );*/
+    }
+}
+
+const NavLink = ({ data }) => {
     return (
-        <li className={"color-primary "+(data.image ? "has-image": "")}>
+        <li className={"color-primary " + (data.image ? "has-image" : "")}>
             <Link to={data.url} title={data.name}>
-                {data.logo &&
-                    <Logo/>
-                }
-                {!data.logo &&
-                <React.Fragment>{data.name}</React.Fragment>
-                }
+                {data.logo && <Logo />}
+                {!data.logo && <React.Fragment>{data.name}</React.Fragment>}
             </Link>
         </li>
     )
