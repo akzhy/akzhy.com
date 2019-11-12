6a1272ad98c4f7733401973f77262e0d80edf521
akzhy <akshay48884@gmail.com>
2019-11-12 18:16:20 +0530

    Added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,17 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import Title from "../components/title"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Layout>
+        <SEO title="404: Not found" />
+        <div className="section container">
+            <Title data="404" />
+            <p className="text-center">The page you are looking for doesn't exist 🙁.</p>
+        </div>
+    </Layout>
 )
 
 export default NotFoundPage
