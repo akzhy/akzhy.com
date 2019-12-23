4b9b33ab7745d810cbb84f996d66f123716b87c8
akzhy <akshay48884@gmail.com>
2019-12-23 17:48:57 +0530

    Ads

--- src/components/adsense.js
+++ src/components/adsense.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Script from 'react-load-script'
 
-export default class Adsense extends React.Component {
+export class AutoAd extends React.Component {
 
     constructor(props){
         super(props);
@@ -35,3 +35,52 @@ export default class Adsense extends React.Component {
         )
     }
 }
+
+
+export class DisplayAd extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            scriptError: false,
+            scriptLoaded: false,
+        }
+    }
+
+    handleScriptError() {
+        this.setState({ scriptError: true })
+    }
+       
+    handleScriptLoad() {
+        this.setState({ scriptLoaded: true })
+    }
+
+    componentDidUpdate(){
+        if(this.state.scriptLoaded){
+            let adsbygoogle;
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        }
+    }
+
+    render (){
+        return(
+            <React.Fragment>
+                <Script
+                url="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+                attributes={{
+                    "data-ad-client": "ca-pub-8484156190843706",
+                    "async": "true"
+                }}
+                onError={this.handleScriptError.bind(this)}
+                onLoad={this.handleScriptLoad.bind(this)}
+                />
+                <ins className="adsbygoogle"
+                data-ad-client="ca-pub-8484156190843706"
+                data-ad-slot="6749903075"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
+            </React.Fragment>
+        )
+    }
+}
+
