fb2cc843900d32166da114ad736ec43259b0798f
Akshay <akshay48884@gmail.com>
2020-06-03 19:16:50 +0530

    Blur up image

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -64,7 +64,7 @@ export const query = graphql`
                             childImageSharp {
                                 fluid(maxWidth: 600) {
                                     src
-                                    ...GatsbyImageSharpFluid_noBase64
+                                    ...GatsbyImageSharpFluid
                                 }
                             }
                         }
