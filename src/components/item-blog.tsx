8f4a37df77874d9f535d501b998f70fc4be522a3
akzhy <akshay48884@gmail.com>
2020-12-12 22:08:55 +0530

    Article tag added

--- src/components/item-blog.tsx
+++ src/components/item-blog.tsx
@@ -13,15 +13,17 @@ interface Props {
 
 export default function BlogItem({id,title,featured_image,date,slug, description}:Props) {
     return (
-        <a className="bg-bg-secondary flex text-fg-primary my-8 rounded overflow-hidden border-4 border-transparent focus:border-primary focus:outline-none duration-300 transition-all hover:border-secondary" title={title} href={`/blog/${slug}`}>
-            <div className="max-w-full w-96 relative flex-shrink-0">
-                {(featured_image && featured_image.fluid) && <Image fluid={featured_image.fluid} />}
-            </div>
-            <div className="p-4 py-8">
-                <h3 className="text-3xl font-black">{title}</h3>
-                <p className="flex mt-4 text-fg-light items-center"><Calendar className="text-primary mr-2"/> {date}</p>
-                <p className="mt-4">{description}</p>
-            </div>
+        <a className="bg-bg-secondary block text-fg-primary my-8 rounded overflow-hidden border-4 border-transparent focus:border-primary focus:outline-none duration-300 transition-all hover:border-secondary" title={title} href={`/blog/${slug}`}>
+            <article className="flex">
+                <div className="max-w-full w-96 relative flex-shrink-0">
+                    {(featured_image && featured_image.fluid) && <Image fluid={featured_image.fluid} />}
+                </div>
+                <div className="p-4 py-8">
+                    <h3 className="text-3xl font-black">{title}</h3>
+                    <p className="flex mt-4 text-fg-light items-center"><Calendar className="text-primary mr-2"/> {date}</p>
+                    <p className="mt-4">{description}</p>
+                </div>
+            </article>
         </a>
     )
 }
--- src/components/item-portfolio.tsx
+++ src/components/item-portfolio.tsx
@@ -17,14 +17,16 @@ export function ShelfItem({id,title,featured_image,date,slug, description}:Props
             <div className="connector w-1/2 top-0 left-1/2 h-11 absolute">
                 <div className="line w-full h-5"></div>
             </div>
-            <a className="bg-bg-secondary flex flex-wrap relative text-fg-primary rounded overflow-hidden border-4 border-transparent focus:border-primary focus:outline-none duration-300 transition-all hover:border-secondary group" title={title} href={`/blog/${slug}`}>
-                <div className="w-full relative flex-shrink-0">
-                    {(featured_image && featured_image.fluid) && <Image fluid={featured_image.fluid} />}
-                </div>
-                <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center flex-col bg-bg-accent content opacity-0 group-hover:opacity-100">
-                    <Title className="text-4xl text-fg-primary">{title}</Title>
-                    <p className="mt-4">{description}</p>
-                </div>
+            <a className="bg-bg-secondary block text-fg-primary rounded overflow-hidden border-4 border-transparent focus:border-primary focus:outline-none duration-300 transition-all hover:border-secondary group" title={title} href={`/blog/${slug}`}>
+                <article className="flex flex-wrap relative">
+                    <div className="w-full relative flex-shrink-0">
+                        {(featured_image && featured_image.fluid) && <Image fluid={featured_image.fluid} />}
+                    </div>
+                    <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center flex-col bg-bg-accent content opacity-0 group-hover:opacity-100">
+                        <Title className="text-4xl text-fg-primary">{title}</Title>
+                        <p className="mt-4">{description}</p>
+                    </div>
+                </article>
             </a>
             <p className="text-fg-light items-center absolute right-0 top-0 date text-sm -mt-12" aria-label={`Published on ${date}`}>{date}</p>
         </div>
