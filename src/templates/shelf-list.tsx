5f52a6c4b83c8456471e2f3f60cf272a3adf4210
akzhy <akshay48884@gmail.com>
2020-12-17 21:02:41 +0530

    Shelf listing page

--- src/templates/shelf-list.tsx
+++ src/templates/shelf-list.tsx
@@ -1,5 +1,61 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import { ShelfContainer } from '../components/item-portfolio'
+import { Section } from '../components/ui'
+import Layout from '../components/layout'
 
-export default function TemplateShelfList() {
-    return <h1>Hello</h1>
+export default function TemplateShelfList({
+    data,
+}: {
+    data: GatsbyTypes.ShelfPageQuery
+}) {
+    return (
+        <Layout
+            seo={{
+                title: 'Shelf',
+                description: 'Shelf Items',
+            }}
+        >
+            <Section title="Shelf">
+                <ShelfContainer
+                    data={data.shelf.edges.map(({ node }) => ({
+                        ...node,
+                        description: node.acf.description || '',
+                        featured_image: node.featuredImage.node?.localFile
+                            ?.childImageSharp as GatsbyTypes.ImageSharp,
+                    }))}
+                />
+            </Section>
+        </Layout>
+    )
 }
+
+export const query = graphql`
+    query ShelfPage {
+        shelf: allWpShelf {
+            edges {
+                node {
+                    id
+                    slug
+                    title
+                    acf {
+                        description
+                    }
+                    date(formatString: "DD MMM YYYY")
+                    featuredImage {
+                        node {
+                            localFile {
+                                childImageSharp {
+                                    fluid {
+                                        src
+                                        ...GatsbyImageSharpFluid
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
