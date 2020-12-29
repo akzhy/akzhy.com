7ed6441c2a49d2a22807d1c51b2995b4662eea77
akzhy <akshay48884@gmail.com>
2020-12-29 19:25:46 +0530

    Absolute imports

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
     siteMetadata: {
         title: `akzhy`,
@@ -32,6 +34,15 @@ module.exports = {
                     'src/__generated__/gatsby-plugin-documents.graphql': true,
                 },
             },
+        },
+        {
+            resolve: 'gatsby-plugin-root-import',
+            options: {
+                components: path.join(__dirname, 'src/components'),
+                utils: path.join(__dirname, 'src/utils'),
+                pages: path.join(__dirname, 'src/pages'),
+                templates: path.join(__dirname, 'src/templates'),
+            }
         },
         `gatsby-plugin-postcss`,
         `gatsby-transformer-sharp`,
