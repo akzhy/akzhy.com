369c1dda3aaae37f2b9020d2bd27d3fe42ed50f5
akzhy <akshay48884@gmail.com>
2021-01-06 22:01:04 +0530

    Pages

--- src/templates/page.tsx
+++ src/templates/page.tsx
@@ -1,5 +1,39 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import TemplateSinglePage from './post'
 
-export default function TemplateSinglePage() {
-    return <h1>Hello</h1>
+export default function PageTemplate({
+    data,
+}: {
+    data: GatsbyTypes.PageSingleQuery
+}) {
+    return <TemplateSinglePage data={data} type="page" />
 }
+
+export const query = graphql`
+    query PageSingle($id: String!) {
+        query: wpPage(id: { eq: $id }) {
+            acf {
+                description
+                metaDescription
+            }
+            databaseId
+            title
+            content
+            date(formatString: "DD MMM YYYY")
+            modified
+            featuredImage {
+                node {
+                    localFile {
+                        childImageSharp {
+                            fluid {
+                                base64
+                                ...GatsbyImageSharpFluid
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+`
