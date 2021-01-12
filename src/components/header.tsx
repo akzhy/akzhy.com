b785c5f6e1f9f3088ac002256448bd16745e90e5
akzhy <akshay48884@gmail.com>
2021-01-12 22:59:19 +0530

    Mobile navigation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Link } from 'gatsby'
 import { useLocation } from '@reach/router'
+import { Menu } from 'react-feather'
+import Sidebar from './sidebar'
+import { useState } from 'react'
 
 export default function Header() {
     const items = [
@@ -26,6 +29,29 @@ export default function Header() {
         },
     ]
 
+    const sidenavItems = [
+        {
+            title: 'Home',
+            href: '/',
+        },
+        {
+            title: 'About',
+            href: '/about',
+        },
+        {
+            title: 'Shelf',
+            href: '/shelf',
+        },
+        {
+            title: 'Blog',
+            href: '/blog',
+        },
+        {
+            title: 'Contact',
+            href: '/contact',
+        },
+    ]
+
     const location = useLocation()
 
     const navItems = items.map((i) => (
@@ -38,10 +64,88 @@ export default function Header() {
         />
     ))
 
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+    const [fixedHeaderShown, setFixedHeaderShown] = useState(true)
+
+    const prevScroll = useRef(0)
+
+    useEffect(() => {
+        if (window.screen.width < 1024) {
+            window.addEventListener('scroll', onScroll)
+            prevScroll.current = window.pageYOffset
+        }
+    }, [])
+
+    const onScroll = () => {
+        if (
+            window.pageYOffset > 300 &&
+            window.pageYOffset < prevScroll.current
+        ) {
+            console.log('okay', fixedHeaderShown)
+            if (!fixedHeaderShown) {
+                setFixedHeaderShown(true)
+            }
+        } else {
+            console.log('yeah', fixedHeaderShown)
+            if (fixedHeaderShown) {
+                setFixedHeaderShown(false)
+            }
+        }
+        prevScroll.current = window.pageYOffset
+    }
+
     return (
-        <nav className="py-8">
-            <ul className="flex items-center justify-center">{navItems}</ul>
-        </nav>
+        <React.Fragment>
+            <nav
+                className="py-4 md:py-6 lg:py-8 fixed lg:relative bg-bg-primary w-full top-0 left-0 z-50 transition-transform duration-300"
+                style={{
+                    ...(!fixedHeaderShown && {
+                        transform: `translate(0, -120px)`,
+                    }),
+                }}
+            >
+                <ul className="hidden lg:flex items-center justify-center">
+                    {navItems}
+                </ul>
+                <div className="lg:hidden flex items-center justify-center relative">
+                    <button
+                        className="absolute left-8 top-0 w-10 h-10 text-fg-primary flex items-center justify-center pt-3"
+                        onClick={() => setSidebarOpen(true)}
+                    >
+                        <Menu size={34} />
+                    </button>
+                    <div>
+                        <div className="logo-container w-32">
+                            <img
+                                src="/images/logo-black.png"
+                                className="logo-dark"
+                                alt="Logo"
+                            />
+                            <img
+                                src="/images/logo-white.png"
+                                className="logo-light"
+                                alt="Logo"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <Sidebar onChange={setSidebarOpen} open={sidebarOpen}>
+                <ul className="">
+                    {sidenavItems.map((item) => (
+                        <NavItem
+                            href={item.href}
+                            title={item.title}
+                            active={
+                                location.pathname.split('/')[1] ===
+                                item.href.split('/')[1]
+                            }
+                            key={`sidenav-${item.href}`}
+                        />
+                    ))}
+                </ul>
+            </Sidebar>
+        </React.Fragment>
     )
 }
 
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,290 @@
+import React, { ReactNode, useEffect, useRef } from 'react'
+import { useState } from 'react'
+
+interface Settings {
+    sensitivity: number
+    overlayColor: string
+    sidebarWidth: string
+    swipeDistance: number
+}
+
+interface State {
+    touchDown: boolean
+    sidebarOpen: boolean
+    progress: number
+    touchX: number
+    touchTime: number
+    lastTouch: number
+    transitionTime: number
+    screenWidth: number
+    opening: boolean
+}
+
+export default function Sidebar({
+    settings = {},
+    onChange,
+    open,
+    children,
+}: {
+    settings?: Partial<Settings>
+    onChange: (v: boolean) => void
+    open: boolean
+    children: ReactNode
+}) {
+    const [state, setState] = useState<State>({
+        touchDown: false,
+        sidebarOpen: false,
+        progress: 0,
+        touchX: 0,
+        touchTime: 0,
+        lastTouch: 0,
+        transitionTime: 0,
+        screenWidth: 400,
+        opening: false,
+    })
+
+    const updateState = (v: Partial<State>) => setState((p) => ({ ...p, ...v }))
+
+    const useSettings: Settings = {
+        sensitivity: 50,
+        overlayColor: '#000',
+        sidebarWidth: '70%',
+        swipeDistance: 40,
+        ...settings,
+    }
+
+    const sidebarParent = useRef<HTMLDivElement>(null)
+    const sidebarOverlay = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        window.addEventListener('touchstart', onTouchStart)
+        window.addEventListener('touchend', onTouchEnd)
+        window.addEventListener('touchmove', onTouchMove)
+        window.addEventListener('resize', onResizeWindow)
+
+        updateState({
+            screenWidth: window.screen.availWidth,
+        })
+        ;() => {
+            window.removeEventListener('touchstart', onTouchStart)
+            window.removeEventListener('touchend', onTouchEnd)
+            window.removeEventListener('touchmove', onTouchMove)
+            window.removeEventListener('resize', onResizeWindow)
+        }
+    }, [])
+
+    useEffect(() => {
+        if ((state.progress === 100) !== open) {
+            if (open) {
+                if (!state.sidebarOpen) {
+                    openSidebar()
+                }
+            } else {
+                if (state.sidebarOpen) {
+                    closeSidebar()
+                }
+            }
+        }
+    }, [open])
+
+    const onTouchStart = (e: TouchEvent) => {
+        let s: Partial<State> &
+            Pick<State, 'touchX' | 'lastTouch' | 'touchTime'> = {
+            touchX: e.touches[0].pageX,
+            lastTouch: e.touches[0].pageX,
+            touchTime: new Date().getTime(),
+        }
+        console.log('touch start', s.touchX, useSettings.sensitivity)
+        if (!state.sidebarOpen && s.touchX < useSettings.sensitivity) {
+            s.opening = true
+            s.touchDown = true
+        } else if (state.sidebarOpen) {
+            s.opening = false
+            s.touchDown = true
+        }
+
+        updateState(s)
+    }
+
+    const onTouchMove = (e: TouchEvent) => {
+        console.log('touch move')
+        if (state.touchDown) {
+            if (state.opening) {
+                if (sidebarParent.current) {
+                    sidebarParent.current.style.transitionDuration = '0s'
+                }
+
+                if (sidebarOverlay.current) {
+                    sidebarOverlay.current.style.transitionDuration = '0s'
+                }
+
+                let progress =
+                    (e.touches[0].pageX / (state.screenWidth * 0.7)) * 100
+                progress = Math.min(100, progress)
+
+                changeSidebarState(
+                    progress === 100 ? 'open' : progress === 0 ? 'close' : '',
+                    {
+                        progress: progress,
+                        lastTouch: e.touches[0].pageX,
+                    }
+                )
+            } else {
+                let diff = state.touchX - e.touches[0].pageX
+                if (diff > 0) {
+                    if (sidebarParent.current) {
+                        sidebarParent.current.style.transitionDuration = '0s'
+                    }
+
+                    if (sidebarOverlay.current) {
+                        sidebarOverlay.current.style.transitionDuration = '0s'
+                    }
+
+                    let progress = 100 - Math.min(100, diff / 2)
+
+                    changeSidebarState(
+                        progress === 100
+                            ? 'open'
+                            : progress === 0
+                            ? 'close'
+                            : '',
+                        {
+                            progress: progress,
+                            lastTouch: e.touches[0].pageX,
+                        }
+                    )
+                }
+            }
+        }
+    }
+
+    const onTouchEnd = (e: TouchEvent) => {
+        if (state.touchDown) {
+            if (state.progress > 80) {
+                openSidebar()
+            } else {
+                closeSidebar()
+            }
+            updateState({
+                touchDown: false,
+            })
+        }
+
+        if (state.lastTouch > state.touchX) {
+            let time = new Date().getTime() - state.touchTime
+            let distance = state.lastTouch - state.touchX
+            let vel = distance / time
+            if (vel > 0.6) {
+                openSidebar()
+            }
+        }
+    }
+
+    const changeSidebarState = (
+        arg: 'open' | 'close' | '',
+        st?: Partial<State>
+    ) => {
+        if (arg === 'open') {
+            updateState({
+                progress: 100,
+                sidebarOpen: true,
+                ...st,
+            })
+            if (onChange) {
+                onChange(true)
+            }
+        } else if (arg === 'close') {
+            updateState({
+                progress: 0,
+                sidebarOpen: false,
+                ...st,
+            })
+            if (onChange) {
+                onChange(false)
+            }
+        } else {
+            updateState(st || {})
+        }
+    }
+
+    const openSidebar = () => {
+        let remaining = 100 - state.progress
+
+        if (sidebarParent.current) {
+            sidebarParent.current.style.transitionDuration = `${
+                remaining / 500
+            }s`
+        }
+
+        if (sidebarOverlay.current) {
+            sidebarOverlay.current.style.transitionDuration = `${
+                remaining / 500
+            }s`
+        }
+        changeSidebarState('open')
+    }
+
+    const closeSidebar = () => {
+        let remaining = state.progress
+
+        if (sidebarParent.current) {
+            sidebarParent.current.style.transitionDuration = `${
+                remaining / 250
+            }s`
+        }
+        if (sidebarOverlay.current) {
+            sidebarOverlay.current.style.transitionDuration = `${
+                remaining / 250
+            }s`
+        }
+        changeSidebarState('close')
+    }
+
+    const onResizeWindow = () => {
+        updateState({
+            screenWidth: window.screen.availWidth,
+        })
+    }
+
+    return (
+        <div className="r-swipe-sidebar-container">
+            <div
+                className="r-swipe-sidebar"
+                ref={sidebarParent}
+                style={{
+                    /* stylelint-disable  */
+                    position: 'fixed',
+                    left: `${state.progress - 100}%`,
+                    width: settings.sidebarWidth,
+                    height: '100%',
+                    top: 0,
+                    zIndex: 9999,
+                    transitionProperty: 'left',
+                    transitionDuration: state.transitionTime + 's',
+                    transitionTimingFunction: 'linear',
+                    transform: 'translate3d(0, 0, 0)',
+                }}
+            >
+                {children}
+            </div>
+            <div
+                className="r-swipe-sidebar-overlay"
+                ref={sidebarOverlay}
+                style={{
+                    position: 'fixed',
+                    top: 0,
+                    bottom: 0,
+                    left: state.progress === 0 ? '-100%' : '0%',
+                    width: '100%',
+                    height: '100%',
+                    background: '#000',
+                    zIndex: 9998,
+                    transitionProperty: 'opacity',
+                    transitionDuration: '0s',
+                    opacity: `${state.progress / 200}`,
+                }}
+                onClick={closeSidebar}
+            ></div>
+        </div>
+    )
+}
