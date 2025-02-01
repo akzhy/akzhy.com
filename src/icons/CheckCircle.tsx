b4e89a4ef78a0e18e140a5b81c9bef545bcd4deb
Akshay <akshay48884@gmail.com>
2025-02-01 17:40:40 +0530

    Add sun, moon, and checkcircle icons

---
+++ src/icons/CheckCircle.tsx
@@ -0,0 +1,21 @@
+import type { IconProps } from "@src/utils/types";
+
+export function CheckCircle({ size = 24, ...props }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      {...props}
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="m9 12 2 2 4-4" />
+    </svg>
+  );
+}
